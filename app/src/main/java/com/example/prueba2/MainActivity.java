4aaee673b3e6679c9546cc2adc76809d587ec913
locoxsoco <carranza.l@pucp.pe>
2019-10-12 02:10:07 +0200

    API Test

--- app/src/main/java/com/example/prueba2/MainActivity.java
+++ app/src/main/java/com/example/prueba2/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.prueba2;
 
+import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +17,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +35,33 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        String URL = "http://jsonplaceholder.typicode.com/posts/42";
+
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest=new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest responseeeeee: ",response.toString());
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest responseSad: ",error.toString());
+
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
     }
 
 }
